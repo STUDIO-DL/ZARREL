ad1af795bd25a4c4978d339c00cce3bbdb158f33
davidcipher <josedavidmba0809@gmail.com>
2026-07-05 06:12:22 +0100

    h

--- src/components/UI/GlassCard.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './GlassCard.module.css';
-
-export default function GlassCard({ children, className = '', variant = 'dark' }) {
-  return (
-    <div className={`${styles.card} ${styles[variant]} ${className}`}>
-      {children}
-    </div>
-  );
-}
